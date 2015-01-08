d719132ae028aac35d39481cd4655476e6f14dc5
Sunil Williams <sunil@sunil.co.nz>
2015-01-09 01:32:26 +1300

    get sourcemaps working

--- front-page.php
+++ front-page.php
@@ -1,4 +1,2 @@
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/page', 'header'); ?>
-  <?php get_template_part('templates/content', 'frontpage'); ?>
-<?php endwhile; ?>
+<?php get_template_part('templates/page', 'header'); ?>
+<?php get_template_part('templates/content', 'frontpage'); ?>
--- templates/page-header.php
+++ templates/page-header.php
@@ -1,5 +1,5 @@
 <div class="page-header">
   <h1>
-    <?php echo roots_title(); ?>
+      in page header
   </h1>
 </div>
