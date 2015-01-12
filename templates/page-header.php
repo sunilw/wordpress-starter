231d38640e14520691e04ba4429dc4ee4115a2cc
Sunil Williams <sunil@sunil.co.nz>
2015-01-13 00:02:33 +1300

    get page title

--- templates/page-header.php
+++ templates/page-header.php
@@ -1,9 +1,7 @@
-<div class="masthead page-header">
-    <div class="container-fluid">
-	<div class="">
-            <h1>
-                In Page Header
-            </h1>
+<div class="container-fluid">
+    <div class="masthead page-header group row">
+        <div class="col-xs-12">
+	    <h1><?php echo get_the_title()  ?></h1>
         </div>
     </div>
 </div>
